f98b8bd5027762885ea7676c851e7bdffbc5f4ba
Aliaksandr Shavialianchyk <aliaksandr_shavialianchyk@epam.com>
2022-08-05 15:27:05 +0300

    added war plugin

--- web/src/main/java/com/epam/esm/config/SpringWebAppInitializer.java
+++ web/src/main/java/com/epam/esm/config/SpringWebAppInitializer.java
@@ -3,10 +3,11 @@ package com.epam.esm.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @EntityScan("com.epam.esm")
 @SpringBootApplication(scanBasePackages = "com.epam.esm")
-public class SpringWebAppInitializer {
+public class SpringWebAppInitializer extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SpringWebAppInitializer.class, args);
     }
