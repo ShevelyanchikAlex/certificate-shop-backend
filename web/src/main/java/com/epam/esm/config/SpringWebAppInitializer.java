8fb090e11b4b6dd5b0d784181831c8ab9735ae8f
Aliaksandr Shavialianchyk <aliaksandr_shavialianchyk@epam.com>
2022-08-07 01:14:34 +0300

    added annotation

--- web/src/main/java/com/epam/esm/config/SpringWebAppInitializer.java
+++ web/src/main/java/com/epam/esm/config/SpringWebAppInitializer.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EntityScan("com.epam.esm")
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = "com.epam.esm")
 public class SpringWebAppInitializer extends SpringBootServletInitializer {
     public static void main(String[] args) {
