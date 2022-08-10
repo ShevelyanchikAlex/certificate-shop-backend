8bc9b30b584e59f8b2603274a1264f6da67bcb84
Aliaksandr Shavialianchyk <aliaksandr_shavialianchyk@epam.com>
2022-08-10 17:26:08 +0300

    temporary secret-configuration init 1.2

--- service/src/main/java/com/epam/esm/service/security/jwt/JwtTokenProvider.java
+++ service/src/main/java/com/epam/esm/service/security/jwt/JwtTokenProvider.java
@@ -3,7 +3,6 @@ package com.epam.esm.service.security.jwt;
 import com.epam.esm.service.exception.JwtAuthenticationException;
 import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -20,12 +19,12 @@ import java.util.Date;
 public class JwtTokenProvider {
     private final UserDetailsService userDetailsService;
 
-    @Value("${security.jwt.token.secret-key}")
-    private String secretKey;
-    @Value("${security.jwt.token.expire-length}")
-    private long validityInMilliseconds;
-    @Value("${security.jwt.token.header}")
-    private String authorizationHeader;
+//    @Value("${security.jwt.token.secret-key}")
+    private String secretKey = "test";
+//    @Value("${security.jwt.token.expire-length}")
+    private long validityInMilliseconds = 604800;
+//    @Value("${security.jwt.token.header}")
+    private String authorizationHeader = "Authorization";
 
     private static final String ROLE = "role";
 
